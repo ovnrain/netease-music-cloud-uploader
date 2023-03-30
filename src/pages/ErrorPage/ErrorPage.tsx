a27ef1de8ba4719bb9cd8653d40cb84940e85881
ovnrain <ovnrain@gmail.com>
2023-03-31 02:29:30 +0800

    show error page message

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,17 @@
-// import styles from './ErrorPage.module.scss';
+import styles from './ErrorPage.module.scss';
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 export interface ErrorPageProps {}
 
 const ErrorPage = (props: ErrorPageProps) => {
-  return <div>您访问的页面不存在</div>;
+  const error = useRouteError() as { statusText?: string; message?: string };
+  return (
+    <div className={styles.container}>
+      <p>错误</p>
+      <p>{error.statusText || error.message}</p>
+    </div>
+  );
 };
 
 export default ErrorPage;
