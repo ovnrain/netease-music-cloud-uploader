b8005612fcb3f9f1ca50a480bc8e8481b7bab45b
ovnrain <ovnrain@gmail.com>
2023-04-04 06:18:06 +0800

    IconFont disabled status

--- src/components/IconFont/IconFont.tsx
+++ src/components/IconFont/IconFont.tsx
@@ -5,13 +5,19 @@ import clsx from 'clsx';
 export interface IconFontProps
   extends DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
   type: string;
+  disabled?: boolean;
 }
 
 const IconFont = forwardRef<HTMLSpanElement, IconFontProps>((props, ref) => {
-  const { className, type, onClick, ...rest } = props;
+  const { className, type, disabled, onClick, ...rest } = props;
 
   return (
-    <span className={clsx(styles.icon, className)} onClick={onClick} ref={ref} {...rest}>
+    <span
+      className={clsx(styles.icon, className, { [styles.disabled]: disabled })}
+      onClick={!disabled ? onClick : undefined}
+      ref={ref}
+      {...rest}
+    >
       <svg className={styles.svg} aria-hidden="true">
         <use xlinkHref={`#${type}`}></use>
       </svg>
