c3f0b1d5a137fc717fbfb932da7c5e6ba764aa31
ovnrain <ovnrain@gmail.com>
2023-04-04 19:52:19 +0800

    app logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import styles from './App.module.scss';
 import { Outlet, Route, Routes } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
 import useUserInfo from './hooks/useUserInfo';
 import Button from './components/Button';
 import { replaceHttpWithHttps } from './utils/common';
+import AppMenu from './components/AppMenu';
+import { setMemoryCookie, setUserCookie } from './utils/cookie';
 
 function App() {
+  const queryClient = useQueryClient();
   const userInfo = useUserInfo();
 
   return (
@@ -23,11 +27,21 @@ function App() {
         </Routes>
         <div className={styles.userInfo}>
           <div className={styles.nickname}>{userInfo.profile.nickname}</div>
-          <img
-            className={styles.avatar}
-            src={replaceHttpWithHttps(userInfo.profile.avatarUrl)}
-            alt="avatar"
-          />
+          <AppMenu
+            onClick={async (key) => {
+              if (key === 'logout') {
+                await setUserCookie('');
+                setMemoryCookie('');
+                queryClient.invalidateQueries(['userInfo']);
+              }
+            }}
+          >
+            <img
+              className={styles.avatar}
+              src={replaceHttpWithHttps(userInfo.profile.avatarUrl)}
+              alt="avatar"
+            />
+          </AppMenu>
         </div>
       </div>
       <div className={styles.content}>
