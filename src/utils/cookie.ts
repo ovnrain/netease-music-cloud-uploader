e48567de4909e917cf08ff508479b4c91377350c
ovnrain <ovnrain@gmail.com>
2023-04-07 23:21:50 +0800

    fix save login status

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,4 +1,5 @@
-import { BaseDirectory, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
+import { BaseDirectory, createDir, exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
+import { appLocalDataDir } from '@tauri-apps/api/path';
 
 let memoryCookie = '';
 
@@ -12,22 +13,31 @@ export function setMemoryCookie(cookie: string) {
 
 export async function getUserCookie() {
   try {
+    if (!(await exists('cookie.txt', { dir: BaseDirectory.AppLocalData }))) {
+      return '';
+    }
     return (
       (await readTextFile('cookie.txt', {
         dir: BaseDirectory.AppLocalData,
       })) || ''
     );
   } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
     return '';
   }
 }
 
 export async function setUserCookie(cookie: string) {
   try {
+    if (!(await exists(await appLocalDataDir()))) {
+      await createDir(await appLocalDataDir());
+    }
     return await writeTextFile('cookie.txt', cookie, {
       dir: BaseDirectory.AppLocalData,
     });
   } catch (e) {
-    //
+    // eslint-disable-next-line no-console
+    console.log(e);
   }
 }
